369e908d73cdbb87ddfa7c9da5408fbcbf5c85d7
jetblacksalvation <coolanimeguy666@gmail.com>
2022-07-22 22:37:03 -0700

    better

--- C Lexical/C Lexical.cpp
+++ C Lexical/C Lexical.cpp
@@ -1,32 +1,23 @@
 #include <iostream>
 #include <vector>
 #include "parse.h"
-#include <string>
-#include <fstream>
-std::fstream file("input.txt");
 
-std::string text;
 int main() {
-	int temp;
+
 	std::vector<std::string> words;
-	std::string tempstr;
+	
 	//parse_words(words, " string 'hello world'; ");
-	// Use a while loop together with the getline() function to read the file line by line
-	int tmep;
-	while (std::getline(file, text)) {
-		tempstr += text;
-		tempstr += " ";//just incase there are no spaces at the last thing 
-		//parse_words(words, tempstr);
-		
-	}
-	std::cout << tempstr << std::endl;
-	parse_words(words, tempstr);
+
+	
+	
+	parse_words(words,"input.txt");
+
+	std::cout << "size of words = " << words.size() << std::endl;
+	//testing stuff 
 	for (int x = 0; x < words.size(); x++) {
-		if (words[x] == "") {
-			words.erase(words.begin()+x);//this function is retarded, why can't a just SAY the index??
-			//and in this use case wouldn't this delete the element after the empty element????
-			//hate this shit
-		}
+
+
 		std::cout << words[x] << std::endl;
 	}
+	
 }
--- C Lexical/parse.h
+++ C Lexical/parse.h
@@ -1,16 +1,40 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+void parse_words(std::vector<std::string>& PushTo,std::string filename) {
 
-void parse_words(std::vector<std::string>& PushTo, std::string text) {
-    
-    size_t pos = 0;
+
+    //variables
+    std::fstream file(filename);
+    std::string tempstr;
+    std::string text;
+    size_t pos = 0;//dummy value, doesnt really matter 
+
+
+
+    while (std::getline(file, tempstr)) {
+        text += tempstr;
+        text += " ";//just incase there are no spaces at the last thing 
+        //parse_words(words, tempstr);
+
+    }//accumilates/pushes all lines of text from file into one string, text
+
+    //finds spaces and ignores/copies to vector 
     while ((pos = text.find(" ")) != std::string::npos) {//npos is just max string size :P
         //also props to the guy i stole the code from, it was dog shit so i changed it
         PushTo.push_back(text.substr(0, pos));
         text.erase(0, pos + 1);
        
         
+    }
+    for (int x = 0; x < PushTo.size(); x++) {//erases all instances of empty
+        if (PushTo[x] == "") {
+            PushTo.erase(PushTo.begin() + x);//this function is retarded, why can't a just SAY the index??
+            //and in this use case wouldn't this delete the element after the empty element????
+            //hate this shit
+        }
     }
     return;
 
